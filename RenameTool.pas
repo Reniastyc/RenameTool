1e7e85a0aad6e509a560d823626674199ed60fc4
Reniastyc de El Magnifico <elmagnifico@vip.qq.com>
2021-06-22 11:38:27 +0800

    1.2 Fix

--- RenameTool.pas
+++ RenameTool.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants, Winapi.ShellAPI, Winapi.Windows,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts, FMX.ExtCtrls, FMX.Memo.Types, FMX.StdCtrls,
-  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.ListBox;
+  FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo, FMX.ListBox, Data.Bind.EngExt, Fmx.Bind.DBEngExt,
+  Data.Bind.Components;
 
 type
   TSubtileRenameTool = class(TForm)
@@ -25,6 +26,17 @@ type
     SpeedButton_Rename: TSpeedButton;
     Label_Files: TLabel;
     Label_Targets: TLabel;
+    Layout_LeftTool: TLayout;
+    ListBox_Files: TListBox;
+    BindingsList1: TBindingsList;
+    Button_LeftSort: TButton;
+    Panel_Left: TPanel;
+    Button_LeftClear: TButton;
+    Layout_ClientTool: TLayout;
+    Panel_Client: TPanel;
+    Button_ClientSort: TButton;
+    Button_ClientClear: TButton;
+    Label_Count: TLabel;
     procedure FormResize(Sender: TObject);
     procedure DropTarget_FilesDropped(Sender: TObject; const Data: TDragObject; const Point: TPointF);
     procedure DropTarget_TargetsDropped(Sender: TObject; const Data: TDragObject; const Point: TPointF);
@@ -35,6 +47,10 @@ type
     procedure SpeedButton_RenameClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure ComboBox_LangChange(Sender: TObject);
+    procedure Button_LeftSortClick(Sender: TObject);
+    procedure Button_LeftClearClick(Sender: TObject);
+    procedure Button_ClientSortClick(Sender: TObject);
+    procedure Button_ClientClearClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -52,19 +68,44 @@ implementation
 procedure TSubtileRenameTool.DropTarget_TargetsDragOver(Sender: TObject; const Data: TDragObject; const Point: TPointF;
   var Operation: TDragOperation);
 begin
-  Operation := TDragOperation.Link;
+  Operation := TDragOperation.Copy;
 end;
 
 procedure TSubtileRenameTool.DropTarget_TargetsDropped(Sender: TObject; const Data: TDragObject; const Point: TPointF);
 var
   i, l: Integer;
 begin
-  Memo_Targets.Lines.Clear;
   l := Length(Data.Files);
   for i := 0 to l - 1 do
   begin
     Memo_Targets.Lines.Add(Data.Files[i]);
   end;
+end;
+
+procedure TSubtileRenameTool.Button_ClientClearClick(Sender: TObject);
+begin
+  Memo_Targets.Lines.Clear;
+end;
+
+procedure TSubtileRenameTool.Button_ClientSortClick(Sender: TObject);
+begin
+  ListBox_Files.Sorted := False;
+  ListBox_Files.Items.Text := Memo_Targets.Lines.Text;
+  ListBox_Files.Sorted := True;
+  Memo_Targets.Lines.Text := ListBox_Files.Items.Text;
+end;
+
+procedure TSubtileRenameTool.Button_LeftClearClick(Sender: TObject);
+begin
+  Memo_Files.Lines.Clear;
+end;
+
+procedure TSubtileRenameTool.Button_LeftSortClick(Sender: TObject);
+begin
+  ListBox_Files.Sorted := False;
+  ListBox_Files.Items.Text := Memo_Files.Lines.Text;
+  ListBox_Files.Sorted := True;
+  Memo_Files.Lines.Text := ListBox_Files.Items.Text;
 end;
 
 procedure TSubtileRenameTool.ComboBox_LangChange(Sender: TObject);
@@ -85,14 +126,13 @@ end;
 procedure TSubtileRenameTool.DropTarget_FilesDragOver(Sender: TObject; const Data: TDragObject; const Point: TPointF;
   var Operation: TDragOperation);
 begin
-  Operation := TDragOperation.Link;
+  Operation := TDragOperation.Copy;
 end;
 
 procedure TSubtileRenameTool.DropTarget_FilesDropped(Sender: TObject; const Data: TDragObject; const Point: TPointF);
 var
   i, l: Integer;
 begin
-  Memo_Files.Lines.Clear;
   l := Length(Data.Files);
   for i := 0 to l - 1 do
   begin
@@ -136,11 +176,12 @@ end;
 
 procedure TSubtileRenameTool.SpeedButton_RenameClick(Sender: TObject);
 var
-  Path, FileName, DestName, TS: string;
+  FileName, DestName, TS: string;
   i, l: Integer;
 begin
   if Memo_Files.Lines.Count <> Memo_Targets.Lines.Count then
   begin
+    ShowMessage(Label_Count.Text);
     Exit;
   end;
   l := Memo_Files.Lines.Count;
@@ -153,12 +194,7 @@ begin
     begin
       if ExtractFilePath(DestName) = '' then
       begin
-        Path := ExtractFilePath(FileName);
-        if not Path.EndsWith('\') then
-        begin
-          Path := Path + '\';
-        end;
-        DestName := Path + DestName;
+        DestName := ExtractFilePath(FileName) + DestName;
       end;
       RenameFile(FileName, DestName);
     end;
